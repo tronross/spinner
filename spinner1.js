2e5b7577c9ab99ff594ae431a726d3bcb4d92c02
tronross <john_ross@analogforever.ca>
2022-10-14 22:28:03 -0400

    Started code

---
+++ spinner1.js
@@ -0,0 +1,17 @@
+setTimeout(() => {
+  process.stdout.write('\r|   ')
+}, 100);
+
+setTimeout(() => {
+  process.stdout.write('\r/   ')
+}, 300);
+
+setTimeout(() => {
+  process.stdout.write('\r-   ')
+}, 500);
+
+setTimeout(() => {
+  process.stdout.write('\r\   ')
+}, 700);
+
+
