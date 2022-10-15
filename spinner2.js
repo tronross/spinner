a7a6a819cc77cf9bb1f1561b4c4b53109012304b
tronross <john_ross@analogforever.ca>
2022-10-14 22:59:21 -0400

    Refactored to object and method

--- spinner2.js
+++ spinner2.js
@@ -1,6 +1,7 @@
 // ASCII animiation using setTimeout, refactored via OOP
 const spinner2 = {
   ascii:        ['\r|   ', '\r/   ', '\r-   ', '\r\   ', '\r|   ', '\r*  '],
+  
   spinner:      function() {  for (let i = 0; i < 6; i++) {
                                 let delay = ((i * 200) +100);
                                 setTimeout(() => {
@@ -9,4 +10,5 @@ const spinner2 = {
                               }
                             }
 };
+
 spinner2.spinner();
