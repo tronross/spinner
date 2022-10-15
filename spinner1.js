8bc2c16682acc1f14e8bd054a967c577a2483733
tronross <john_ross@analogforever.ca>
2022-10-14 22:37:29 -0400

    finished ascii animation

--- spinner1.js
+++ spinner1.js
@@ -1,17 +1,73 @@
+// ASCII animiation using setTimeout
+
 setTimeout(() => {
-  process.stdout.write('\r|   ')
+  process.stdout.write('\r|   ');
 }, 100);
 
 setTimeout(() => {
-  process.stdout.write('\r/   ')
+  process.stdout.write('\r/   ');
 }, 300);
 
 setTimeout(() => {
-  process.stdout.write('\r-   ')
+  process.stdout.write('\r-   ');
 }, 500);
 
 setTimeout(() => {
-  process.stdout.write('\r\   ')
+  process.stdout.write('\r\   ');
 }, 700);
 
+setTimeout(() => {
+  process.stdout.write('\r|   ');
+}, 900);
 
+setTimeout(() => {
+  process.stdout.write('\r/   ');
+}, 1100);
+
+setTimeout(() => {
+  process.stdout.write('\r-   ');
+}, 1300);
+
+setTimeout(() => {
+  process.stdout.write('\r\   ');
+}, 1500);
+
+setTimeout(() => {
+  process.stdout.write('\r|   ');
+}, 1700);
+
+setTimeout(() => {
+  process.stdout.write('\r/   ');
+}, 1900);
+
+setTimeout(() => {
+  process.stdout.write('\r-   ');
+}, 2100);
+
+setTimeout(() => {
+  process.stdout.write('\r\   ');
+}, 2300);
+
+setTimeout(() => {
+  process.stdout.write('\r|   ');
+}, 2500);
+
+setTimeout(() => {
+  process.stdout.write('\r/   ');
+}, 2700);
+
+setTimeout(() => {
+  process.stdout.write('\r-   ');
+}, 2900);
+
+setTimeout(() => {
+  process.stdout.write('\r\   ');
+}, 3100);
+
+setTimeout(() => {
+  process.stdout.write('\r|   ');
+}, 3300);
+
+setTimeout(() => {
+  process.stdout.write('\r*  ');
+}, 3500);
